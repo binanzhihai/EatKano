191804ebd4521b5daf53e23f874fab6e2c542118
binanzhihai <98465752+binanzhihai@users.noreply.github.com>
2022-01-27 20:25:22 +0800

    Update index.js

--- static/index.js
+++ static/index.js
@@ -401,9 +401,9 @@ const MODE_NORMAL = 1, MODE_ENDLESS = 2, MODE_PRACTICE = 3;
         }
         // TODO 添加反馈
         if (mode === MODE_ENDLESS) return '';
-        if (score <= 49) return '试着好好练一下？';
-        if (score <= 99) return 'TCL';
-        if (score <= 149) return 'TQL';
+        if (score <= 49) return '“我要认真了！”';
+        if (score <= 99) return '就这？';
+        if (score <= 149) return '“二百五！”';
         if (score <= 199) return '您';
         return '人？';
     }
